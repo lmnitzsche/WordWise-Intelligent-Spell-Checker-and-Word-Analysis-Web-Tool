9e11a4221a2b8d0edc83805e6ad941e5998f1146
lmnitzsche <logannitzsche1@gmail.com>
2025-01-23 01:44:55 -0600

    Updated title

--- REACT PLAYGROUND/src/components/Header.js
+++ REACT PLAYGROUND/src/components/Header.js
@@ -3,8 +3,8 @@ import "./Header.css";
 
 const Header = () => (
   <header className="header">
-    <h1 className="title">Word Analyzer</h1>
-    <p className="subtitle">Analyze 2,000,000+ external resources for over 500,000 words</p>
+    <h1 className="title">Word Wise</h1>
+    <p className="subtitle">Analyze 1,500,000+ external resources for over 370,000 words</p>
   </header>
 );
 
